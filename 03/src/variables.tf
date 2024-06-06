6304f47f26832d3d473350812b5abb66c8e0a34d
berezhok <berezhok1988@gmail.com>
2024-06-06 16:43:50 +0300

    03

--- 03/src/variables.tf
+++ 03/src/variables.tf
@@ -1,16 +1,19 @@
 ###cloud vars
 variable "token" {
   type        = string
+  default     = "###"
   description = "OAuth-token; https://cloud.yandex.ru/docs/iam/concepts/authorization/oauth-token"
 }
 
 variable "cloud_id" {
   type        = string
+  default = "b1gls123arn38ddjtrd9"
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
 }
 
 variable "folder_id" {
   type        = string
+  default = "b1g8qo5pjsu3j0o951sk"
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
 }
 
